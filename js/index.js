a335a295827b7766d0efc60b8bce166e3767b87d
Joel Ramos <ramosj3@wwu.edu>
2021-08-18 00:36:31 -0700

    adding projects page to website

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+        .pipe(sass())
+        .pipe(gulp.dest('test-css'))
+})
